9f7d3596e962cf1ada94a3444e38b9e2d52c3e63
aledepetris <aledepetris@outlook.com>
2024-08-30 15:52:57 -0300

    minimum change for label text

--- src/app/main/pages/alumns/alumn-new/alumn-new.component.ts
+++ src/app/main/pages/alumns/alumn-new/alumn-new.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-alumn-new',
   templateUrl: './alumn-new.component.html',
   styleUrl: './alumn-new.component.scss'
 })
-export class AlumnNewComponent {
+export class AlumnNewComponent implements OnInit {
 
   selectedState: any = null;
+
+  ngOnInit(): void {
+
+  }
 
   dropdownItems = [
     { name: 'Option 1', code: 'Option 1' },
